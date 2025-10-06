f14c81bd94bc0fc503bac4aba8c517a94d374e2e
Tom Claes <tom.claes82@gmail.com>
2025-10-06 22:55:56 +0200

    Deleted +page.svelte

--- src/routes/test/+page.svelte
+++
@@ -1,9 +0,0 @@
-<svelte:head>
-	<title>Test</title>
-	<meta name="description" content="Test your skills" />
-</svelte:head>
-
-<div>
-  <h1>Test your skills</h1>
-  <h2>Select word set to practice</h2>
-</div>
