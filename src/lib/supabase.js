131eae5adca0c0c1d232255d4f9e03c2b870cc2b
Tom Claes <tom.claes82@gmail.com>
2025-11-11 20:17:19 +0100

    Updated supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,22 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
-import { browser } from '$app/environment';
-
-let supabaseUrl = '';
-let supabaseKey = '';
-
-if (browser) {
-	supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-	supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-}
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
 export function getSupabase() {
-	if (!browser) {
-		return createClient(
-			process.env.VITE_SUPABASE_URL || '',
-			process.env.VITE_SUPABASE_ANON_KEY || ''
-		);
-	}
-	return createClient(supabaseUrl, supabaseKey);
+	return createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
 }
 
-export const supabase = browser ? getSupabase() : null;
+export const supabase = getSupabase();
